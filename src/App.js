ff1b034e897bd3bfc68c49511b30fac22144c8f6
Pablo Cabrol <pablo.cabrol@softvision.com>
2024-10-03 14:37:48 -0300

    app text change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Github Workflow by Pablo Cabrol.</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Creating a github actions workflow to build and deploy and react app
+          into AWS s3 bucket.
         </p>
         <a
           className="App-link"
